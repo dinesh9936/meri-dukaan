3984677c45c844966e979ffae6539f727c27d5a2
dinesh9936 <dineshkumarcse0060@gmail.com>
2025-03-25 11:07:00 +0530

    initializing meri dukaan backend

---
+++ src/main/java/com/meridukaan/meri_dukaan/MeriDukaanApplication.java
@@ -0,0 +1,13 @@
+package com.meridukaan.meri_dukaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeriDukaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeriDukaanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meridukaan/meri_dukaan/MeriDukaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meridukaan.meri_dukaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeriDukaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
