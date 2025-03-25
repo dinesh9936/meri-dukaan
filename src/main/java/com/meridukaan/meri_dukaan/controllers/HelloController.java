01e403e7dd94f817076b4c1621e725eb0c8e695a
dinesh9936 <dineshkumarcse0060@gmail.com>
2025-03-25 11:22:31 +0530

    Create HelloController.java
    added say hello api for testing purpose

---
+++ src/main/java/com/meridukaan/meri_dukaan/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.meridukaan.meri_dukaan.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from meri dukaan backend.";
+    }
+    
+    
+}
